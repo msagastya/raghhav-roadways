8a6656668605de5dcc348e6beb46ec4b25a02b5b
msagastya <ms.rudra.agastya@gmail.com>
2026-04-30 07:25:38 +0530

    chore: remove obsolete project clutter

--- .claude/worktrees/nifty-swirles-67477d/frontend/src/store/uiStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand';
-
-const useUIStore = create((set) => ({
-  sidebarOpen: true,
-  toggleSidebar: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
-  setSidebarOpen: (open) => set({ sidebarOpen: open }),
-}));
-
-export default useUIStore;
